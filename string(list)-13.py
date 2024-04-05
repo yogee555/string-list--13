2ebd2cc7147fcc7b148df1f756e729980a06bd02
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-04-05 11:25:48 +0530

    Add files via upload

---
+++ string(list)-13.py
@@ -0,0 +1,5 @@
+# Reversing element of list
+L1 = [1,'a',2,'b',3,'c']     # L1 for store the value
+print(L1)                    # print L1
+L1.reverse()                 # reverse formula for use
+print(L1)                    # print reverse order
